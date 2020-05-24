bc664228c0391530e95adb55e154e59e44ff6747
PandaPlaysAll <josh.piper@live.com.au>
2020-05-24 17:58:44 +1000

    Added scripts

--- OpenChromeTabs.ps1
+++ OpenChromeTabs.ps1
@@ -1,25 +1,30 @@
 #Ask would you lik to change the links. Add the urls
 #read from CSV file
-
+cd 'C:\Users\Joshua\Desktop\Scripts PS'
 $stayOpen = $false
 while ($stayOpen -ne $true) {
-    $Additions = Read-Host 'Type anything to add urls. Or enter to continue' 
-    if ($Additions.length -gt 0) 
+    $Additions = Read-Host 'Type 1 to define new list, type 2 to open default urls, or anything else to open last list' 
+    if ($Additions.Equals("1")) 
     {
         $url = ""
         $AmountOfUrl = Read-Host 'How many urls do you want to open'
+        Clear-Content ".\writeTo.txt"
         $int = 1
+
         DO
     {
         $text = Read-Host 'Enter url'
         $url += $text + ", "
         $int++
+        
+
+
          
-    } while ($int -le [int]$AmountOfUrl)
+    } while ($int -eq [int]$AmountOfUrl)
     #How many urls
     #Enter url'
     #Open url's
-    } else {
+    } elseif ($Additions.Equals("1")){
 
     $url = 
     "https://docs.google.com/document/d/1mILlJMh5C0PDQKpf-h5V-VIMCME1rTkn9GFwPETcs2M/edit#",
@@ -34,6 +39,8 @@ while ($stayOpen -ne $true) {
     $creativeCloud = Get-Process -name 'Creative Cloud'
     $WINWORD = Get-Process -name WINWORD 
     
+    } else {
+    $url = Get-Content ".\writeTo.txt"
     }
     #parametres
     if ($creativeCloud) 
@@ -45,12 +52,10 @@ while ($stayOpen -ne $true) {
     {
         $WINWORD.CloseMainWindow()
     }
+    $url | Add-Content -Path writeTo.txt
     Start-Process "chrome.exe" -ArgumentList $url
 
     $stayOpen = $true
 }
 
-# Sort Desktop by name alphabetically?
-# Popup to tell you what applications you want to close?
-# for it to save to the file so it remembers that
 
